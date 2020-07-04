f41057c3446962c33939bc2a2c5bb339e5eb54a2
gempir <gempir.dev@gmail.com>
2020-07-04 15:35:13 +0200

    basic emote implementation, very slow so far will need to find a better way to render

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,14 +1,123 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, TextInput, FlatList } from 'react-native';
+import { ChatClient, PrivmsgMessage, TwitchEmote } from "dank-twitch-irc";
+import reactStringReplace from "react-string-replace";
+import AutoHeightImage from 'react-native-auto-height-image';
 
 import { Text, View } from '../components/Themed';
 
-export default function ChatScreen() {
+interface IProps {
+}
+
+interface IState {
+    messages: Array<PrivmsgMessage>;
+    channels: Array<string>;
+    addChannel: string;
+}
+
+function ChatMessage(props: { message: PrivmsgMessage }) {
+    const msg = props.message;
+
+    const replacements: Array<TwitchEmote> = [];
+
+    for (const emote of msg.emotes) {
+        replacements.push({ ...emote, endIndex: emote.endIndex + 1 });
+    }
+
+    const func = (match: string, index: number, offset: number) => {
+        for (const replacement of replacements) {
+            if (replacement.startIndex === offset) {
+                return <AutoHeightImage
+                    key={index}
+                    width={28}
+                    source={{
+                        uri: `https://static-cdn.jtvnw.net/emoticons/v1/${replacement.id}/1.0`,
+                    }}
+                />
+            }
+        }
+
+        return match;
+    };
+
+    const codes = replacements.map(emote => emote.code.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")).join("|");
+    const renderMessage = reactStringReplace(msg.messageText, new RegExp(`(${codes})`, "g"), func);
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
-        </View>
+        <Text style={styles.chatMessage}><b style={{ color: props.message.colorRaw }}>{props.message.displayName}</b>: {renderMessage}</Text>
     );
+}
+
+export default class ChatScreen extends React.Component<IProps, IState> {
+    state = {
+        channels: [],
+        messages: [],
+        addChannel: "",
+    }
+
+    messageBuffer: number = 200;
+
+    client: ChatClient;
+
+    constructor(props: object) {
+        super(props);
+
+
+        this.client = new ChatClient({ connection: { type: "websocket", secure: true } });
+
+        this.client.on("ready", () => console.log("Successfully connected to chat"));
+        this.client.on("close", (error: any) => {
+            if (error != null) {
+                console.error("Client closed due to error", error);
+            }
+        });
+
+        this.client.on("PRIVMSG", (msg) => {
+            this.setState({
+                messages: [...this.state.messages.slice(this.state.messages.length - this.messageBuffer - 1), msg],
+            });
+        });
+
+        this.client.connect();
+
+        setTimeout(() => {
+            this.setState({
+                addChannel: "gempir"
+                // addChannel: "twitchmedia_qs_10"
+            }, this.addChannel);
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <View style={styles.container} >
+                {this.state.channels.length > 0 && <FlatList inverted data={this.state.messages.reverse()} renderItem={({ item }) => <ChatMessage message={item} />}
+                    keyExtractor={(item: PrivmsgMessage) => item.messageID} style={styles.scrollView}></FlatList>}
+                {this.state.channels.length === 0 && <>
+                    <TextInput placeholder="channel" onChangeText={this.handleAddChannelChange} style={styles.textInput} value={this.state.addChannel} />
+                    <Button title="Add channel" onPress={this.addChannel} />
+                </>}
+            </View>
+        );
+    }
+
+    addChannel = () => {
+        if (this.state.addChannel === "") {
+            return;
+        }
+
+        this.client.join(this.state.addChannel);
+        this.setState({
+            channels: [...this.state.channels, this.state.addChannel],
+            addChannel: "",
+        });
+    }
+
+    handleAddChannelChange = (text: string) => {
+        this.setState({
+            addChannel: text,
+        });
+    }
 }
 
 const styles = StyleSheet.create({
@@ -17,13 +126,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
+    emote: {
     },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
+    scrollView: {
+        width: "100%",
+    },
+    chatMessage: {
+        padding: 3,
+    },
+    textInput: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        width: "80%",
+        padding: 10,
+        marginBottom: 20
     },
 });
