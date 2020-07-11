ff732cf54c8f1d834600e3eb51ae5f586a84678c
gempir <gempir.dev@gmail.com>
2020-07-11 14:36:33 +0200

    refactor app more and remove expo assets from repo

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import Main from "./navigation/Main";
+import { createInitialState, reducer } from "./store/store";
+
+const store = createStore(reducer, createInitialState(), applyMiddleware(thunk));
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
@@ -13,7 +20,10 @@ export default function App() {
     } else {
         return (
             <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
+                <Provider store={store}>
+                    <StatusBar />
+                    <Main colorScheme={colorScheme} />
+                </Provider>
             </SafeAreaProvider>
         );
     }
